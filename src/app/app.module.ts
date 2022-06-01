9ccb0894f7b3178acc77ef50b631d8ae5b414eff
MaryAnzh <MaryAnzh@mail.ru>
2022-06-01 23:11:49 +0400

    refactor: install @ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,15 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HTTPClientInterceptor } from './core/interceptor/http-client.interceptor';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+
 
 import { LoginComponent } from './auth/pages/login/login.component';
 import { RegistrationComponent } from './auth/pages/registration/registration.component';
 import { EditProfileComponent } from './auth/pages/edit-profile/edit-profile.component';
+import { reducers, metaReducers } from './redux/reducers';
 
 @NgModule({
   declarations: [
@@ -22,6 +27,11 @@ import { EditProfileComponent } from './auth/pages/edit-profile/edit-profile.com
     BrowserModule,
     AppRoutingModule,
     CoreModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers
+    }),
+    //StoreModule.forRoot(reducers),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
   ],
   providers: [
     {
